5fc03ffb61713e84ca51b5516db4fae608d8ca42
KoreanCow <tyunnon1257@naver.com>
2019-10-29 17:03:21 +0900

    Crawling

--- readMe.md
+++ readMe.md
@@ -1,6 +1,9 @@
 # Node.js Crawling
 
-### cheerio-httpcli 모듈 사용
-### 크롤링 시 인코딩 문제 iconv 모듈 사용할 것 
-###  ㄴ npm install -global --production windows-build-tools
+### 네이버 영화 순위 
+
+##### cheerio 모듈 사용
+##### 크롤링 시 인코딩 문제 iconv 모듈 사용할 것 
+##### iconv 모듈 에러시 밑에 구문 실행
+#####  ㄴ npm install -global --production windows-build-tools
      
--- routes/use_cheerio.js
+++ routes/use_cheerio.js
@@ -4,19 +4,21 @@ const router = express.Router();
 const request = require('request');
 const cheerio = require('cheerio');
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const Iconv = require('iconv').Iconv;
 
-router.get('/cheerioTest', function(req, res, next) {
+router.get('/', function(req, res, next) {
   let url = "http://movie.naver.com/movie/sdb/rank/rmovie.nhn";
 
-  request(url, function(error, response, body){
+  request({url, encoding: 'binary'}, function(error, response, body){
     // console.log(body) 파싱 내용 확인
     // cheerio 모듈은 jQuery와 유사 -> HTML요소에 접근하기위해 셀렉터 사용
+
+    let convertedCon = new Buffer(body, 'binary')
+    iconv = new Iconv('euc-kr', 'UTF8');
+    convertedCon = iconv.convert(convertedCon).toString()
     
     let resultArr = [];
-    const $ = cheerio.load(body);
+    const $ = cheerio.load(convertedCon);
     // 백에서 jQuery 셀렉터를 사용할 수 있도록 jQuery 기호인
     // $변수에 cheerio.load() 메소드를 호출한 값을 할당
     // 메소드의 인자로 request(url)의 실행 데이터를 body넘겨줌
@@ -29,8 +31,9 @@ router.get('/cheerioTest', function(req, res, next) {
       resultArr.push(colArr[i].children[1].attribs.title)
       // 모든 tit3 클래스를 resultArr에 담음
     }
-
     res.json(resultArr);
   })
+  // res.render('layout');
 })
+
 module.exports = router;
